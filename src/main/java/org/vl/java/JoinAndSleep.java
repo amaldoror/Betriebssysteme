6c2bb9ae02d5caa980e6b1b3d0db29ae8d078838
Adrian Morgenthal <Adrian.Morgenthal@haw-hamburg.de>
2024-07-12 22:13:02 +0200

    Refactor project structure

--- src/main/java/org/vl/java/JoinAndSleep.java
+++ src/main/java/org/vl/java/JoinAndSleep.java
@@ -1,5 +1,23 @@
-package org.vl;
-
+package org.vl.java;
+/**
+ * <p><b><u>JoinAndSleep</u></b></p>
+ * Die geforderte Aufgabe soll in einer Klasse JoinAndSleep (extends Thread) implementiert werden:<br>
+ * Der Konstruktor der Klasse erlaubt es die Schlafzeit zu übergeben und eine Referenz auf einen anderen Thread.
+ * Dies ist der Thread auf den gewartet werden soll.<br>
+ * run() Methode: <br>
+ * Diese Methode implementiert die oben genannte Semantik zum Warten und Schlafen.
+ * Falls ein Thread gegeben ist, so soll auf sein Ende gewartet werden.
+ * Anschließend soll eine bestimmte Zeit geschlafen werden.
+ * Fügen Sie zwischen allen Schritten Konsolenausgaben ein, um den Fortschritt zu kontrollieren.
+ * Geben Sie hier immer auch den aktuellen Thread aus!<br>
+ * main() Methode<br>
+ * Erzeuge Thread 3: Er soll auf keinen Thread warten und dann4000ms schlafen<br>
+ * Erzeuge Thread 2: Er soll auf Thread 3 warten und dann 3000ms schlafen<br>
+ * Erzeuge Thread 1: Er soll auf Thread 2 warten und dann 2000ms schlafen<br>
+ * Starten Sie Thread 1<br>
+ * Starten Sie Thread 2<br>
+ * Starten Sie Thread 3<br>
+ */
 public class JoinAndSleep extends Thread {
     private int sleepTime; // Zeit, die der Thread schlafen soll
     private Thread waitForThread; // Referenz auf den Thread, auf den gewartet werden soll
@@ -11,7 +29,7 @@ public class JoinAndSleep extends Thread {
     }
 
     @Override
-    public void start() {
+    public void run() {
         try {
             // Wenn ein Thread zum Warten gegeben ist, warte darauf
             if (waitForThread != null) {
